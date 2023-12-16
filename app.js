c4f668071cc1cb4177a2e420ec0c7d1e1c6de064
sohajawaid <sohamemon24@gmail.com>
2023-12-16 12:14:27 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,135 @@
+//chapter 1
+//var email = prompt ("Enter your email")
+//var pasword = prompt("Enter yor pasword")
+//alert ("Thank you")
+
+//chapter 2
+//var personName = "saboor"
+//alert (personName)
+//var teamName ="Tagra XI"
+//alert (teamName )
+//var bestWomen ="Saboor"
+
+//chapter 3
+var caseQty = 144
+var NUM ="9"
+var marks = 74
+marks = +2
+var merchTotal = 100;
+var shppingCharge = 10;
+var ordertotal = merchTotal+ shppingCharge;
+console.log(ordertotal)
+
+var value = 58
+value = 58+58
+console.log(value)
+
+//chapter 4
+var productCost = 3.45
+var nameOfBand;
+var topi = 250;
+var firstNameLastName = "Soha Nagda"
+
+//legal
+// person
+//product_cost
+
+//illegal
+//personname
+//10 person
+//product - cost
+
+//chapter 5
+//modulus operator "%"
+//var number = 20%6
+//console.log(number)
+
+
+//chapter 6
+var x = 50;
+var y = x++;
+console.log (y)
+
+var y = 50;
+var z = --y;
+console.log(z)
+
+var a = 30;
+var b = a++;
+console.log (a,b)
+console.log (b,a)
+
+//chapter 7
+var calculateNum = 2 + (2*6);
+console.log(calculateNum)
+
+var calculateNum =(2+2)*6;
+console.log (calculateNum)
+
+var calculateNum =((2+2)*4)+2;
+console.log (calculateNum)
+
+var calculateNum =(2+2)*(4+2);
+console.log (calculateNum)
+
+var a = 2+2*4+10
+console.log(a)
+
+var x = 4/2*4;
+console.log (x)
+
+//chapter 8
+var num = "2" +"2"
+marks = +2
+alert("Hello Dolly")
+alert("33"+3)
+
+var a = "pakistan"
+var b = "zindabad"
+var c =(a+b)
+console.log (c)
+
+//chapter 9
+//var firstName = prompt("Enter your fiest name")
+//var agenumber = prompt("enter your age")
+
+//chapter 10
+var city = "karachi"
+if (city ="karachi"){
+    console.log("the city of lights")
+}
+//2.This is the first line of an if statement: if (x === y)
+var x = 13
+var y = 13
+var z =(x === y)
+if (x === y){
+      console.log(z)
+}
+var city = 10010
+if (city === 10010){
+    console.log("karachi")
+}else{
+    console.log ("please write correct city")
+}
+    
+//chapter 15
+//Array
+// Array = []
+var arr = ["c"]
+console.log (arr)
+
+var arr =["h","i","j","k"]
+console.log(arr[2])
+
+var arr=["h","i","j","k","l","m","n","o"]
+arr.push("p")
+console.log(arr)
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+arr.splice(0,1)
+console.log(sizes)
+
+
+//chapter 17-20
+for(var i = 0; 10; i++)
+console.log(i)
